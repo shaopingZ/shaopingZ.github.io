c802883544845946046220d8e165d04b38f5075b
shaopingZ <131336162+shaopingZ@users.noreply.github.com>
2024-08-26 22:25:37 +0800

    Create custom_zsp.js

---
+++ assets/js/custom_zsp.js
@@ -0,0 +1,8 @@
+<!-- jQuery -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+
+<!-- Popper.js -->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+
+<!-- Bootstrap JS -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
